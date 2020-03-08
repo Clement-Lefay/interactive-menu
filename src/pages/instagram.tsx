aa1563b4294fb56adfdbf50532882763d8909fb3
Clement LE FAY <clement.le.fay@gmail.com>
2020-03-08 18:53:28 -0400

    Remove all insta stuff and upgrade dependancies

--- src/pages/instagram.tsx
+++ src/pages/instagram.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
-import { config, animated, useSpring, useTrail } from 'react-spring'
-import { Flex } from '../elements'
-import Layout from '../components/layout'
-import { ChildImageSharp } from '../types'
-import SEO from '../components/SEO'
-import Heart from '../heart.svg'
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+import { config, animated, useSpring, useTrail } from "react-spring"
+import { Flex } from "../elements"
+import Layout from "../components/layout"
+import { ChildImageSharp } from "../types"
+import SEO from "../components/SEO"
+import Heart from "../heart.svg"
 
 const Grid = styled(animated.div)`
   display: grid;
@@ -113,32 +113,28 @@ type Props = {
   }
 }
 
-const Instagram: React.FunctionComponent<Props> = ({
-  data: {
-    instagram: { nodes: instagram },
-  },
-}) => {
+const Instagram: React.FunctionComponent<Props> = () => {
   const pageAnimation = useSpring({
     config: config.default,
     from: { opacity: 0 },
-    to: { opacity: 1 },
+    to: { opacity: 1 }
   })
 
-  const trail = useTrail(instagram.length, {
-    config: {
-      mass: 1,
-      tension: 210,
-      friction: 23,
-    },
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  })
+  // const trail = useTrail(instagram.length, {
+  //   config: {
+  //     mass: 1,
+  //     tension: 210,
+  //     friction: 23,
+  //   },
+  //   from: { opacity: 0 },
+  //   to: { opacity: 1 },
+  // })
 
   return (
     <Layout color="#3F4F67">
       <SEO title="Instagram | Jodie" />
       <Grid style={pageAnimation}>
-        {trail.map((style, index) => {
+        {/* {trail.map((style, index) => {
           // Grab everything before the first hashtag (because I write my captions like that)
           const post = instagram[index]
           const title = post.caption ? post.caption.split('#')[0] : ''
@@ -159,7 +155,7 @@ const Instagram: React.FunctionComponent<Props> = ({
               </Content>
             </Item>
           )
-        })}
+        })} */}
       </Grid>
     </Layout>
   )
@@ -167,22 +163,25 @@ const Instagram: React.FunctionComponent<Props> = ({
 
 export default Instagram
 
-export const query = graphql`
-  query Instagram {
-    instagram: allInstaNode(sort: { fields: timestamp, order: DESC }, limit: 30) {
-      nodes {
-        caption
-        id
-        timestamp
-        likes
-        localFile {
-          childImageSharp {
-            fluid(quality: 100, maxWidth: 600, maxHeight: 600) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query Instagram {
+//     instagram: allInstaNode(
+//       sort: { fields: timestamp, order: DESC }
+//       limit: 30
+//     ) {
+//       nodes {
+//         caption
+//         id
+//         timestamp
+//         likes
+//         localFile {
+//           childImageSharp {
+//             fluid(quality: 100, maxWidth: 600, maxHeight: 600) {
+//               ...GatsbyImageSharpFluid_withWebp
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
