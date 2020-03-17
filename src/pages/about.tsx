251392cf228aa3e8ccfa26d760cbd52cb0a79245
Clement LE FAY <clement.le.fay@gmail.com>
2020-03-16 22:44:51 -0400

    Update content of the about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,12 +13,22 @@ const About = () => {
 
   return (
     <Layout>
-      <SEO title="About | Jodie" desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects." />
+      <SEO title="About | Clement" desc="Hi. I'm Clement and this is an experiment of an interractive mennu" />
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
-        <h1>Hi. I'm LekoArts!</h1>
+        <h1>Hi I'm Clement =)</h1>
         <p>
-          You can visit my <a href="https://www.lekoarts.de/en">website</a> or my other{' '}
-          <a href="https://gatsby-starter-portfolio.netlify.com">Gatsby projects</a>.
+          An international Frenchman who likes to help people making their life easier, both physicaly and digitaly. When something has to
+          be done, I don"t hesitate to put my hands on it and do it. Either building IKEA furnitures, speaking out loud hard topics that
+          slows down our team, making smalls tools that simplify some redundant tasks, refining the project I am working on.
+        </p>
+        <p>
+          The environment around me is very important and I try to spend a bit of my energy to make it good, even improve it sometimes! Just
+          ask any of my previous teammate, you will not be decepointed!
+        </p>
+        <p>Of course, this is just a little bit of my personality and what people, both professionally and personally, can appreciate.</p>
+        <p>
+          Now, I'm in Canada to try to see if this country is as good as every canadian people told me, and maybe make my new life here who
+          knows =)
         </p>
       </AnimatedBox>
     </Layout>
